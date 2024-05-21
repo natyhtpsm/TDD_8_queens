453e63c0c2b67eec9f42aabd5e307c3977bae4ac
Nathália P. Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-05-21 20:25:23 -0300

    Update: queens header

--- rainhas.hpp
+++ rainhas.hpp
@@ -1,7 +1,21 @@
-#ifndef RAINHAS_HPP
-#define RAINHAS_HPP
+#ifndef RAINHAS_HPP_
+#define RAINHAS_HPP_
 
-int verifica_solucao(const char* tabuleiro[8]);
-void salva_ataques(const char* tabuleiro[8], const char* filename);
+#include <string>
+#include <vector>
+#include <utility>
 
-#endif // RAINHAS_HPP
+const int TAMANHO = 8;
+const bool VERBOSE = false;
+
+int verifica_solucao(const std::string& arquivo);
+bool arquivo_para_matriz(const std::string& arquivo_txt, char tabuleiro[TAMANHO][TAMANHO]);
+bool verifica_rainhas(char tabuleiro[TAMANHO][TAMANHO]);
+int ataques(char tabuleiro[TAMANHO][TAMANHO]);
+std::string cria_nome_arquivo(std::string diretorio);
+std::vector<std::pair<int, int>> posicoes_rainhas(char tabuleiro[TAMANHO][TAMANHO]);
+std::vector<std::string> possiveis_ataques(const std::vector<std::pair<int, int>>& rainhas);
+bool registra_ataques(std::string nome_arquivo, const std::vector<std::string>& ataques);
+void imprime_tabuleiro(char tabuleiro[TAMANHO][TAMANHO]);
+
+#endif  // RAINHAS_HPP_
