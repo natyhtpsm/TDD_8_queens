4fba70e9578b7fefe303b1633fa510a7f5a32d71
Nathália P. Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-05-21 23:35:23 -0300

    Feat: Test case 3
    - Tabuleiro invalido  (menos de 8 rainhas)

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -29,3 +29,17 @@ TEST_CASE("Tabuleiro inválido (mais de 8 rainhas)", "[verificar_solucao]") {
     };
     REQUIRE(verificar_solucao(tabuleiro) == -1);
 }
+
+TEST_CASE("Tabuleiro inválido (menos de 8 rainhas)", "[verificar_solucao]") {
+    const char tabuleiro[TAMANHO][TAMANHO] = {
+        {'0', '0', '0', '0', '0', '0', '0', '0'},
+        {'0', '1', '0', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '1', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0', '1', '0'},
+        {'0', '0', '1', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0', '0', '1'},
+        {'0', '0', '0', '0', '0', '1', '0', '0'},
+        {'1', '0', '0', '0', '0', '0', '0', '0'}
+    };
+    REQUIRE(verificar_solucao(tabuleiro) == -1);
+}
