123f4ac0075139d913977bb359ba70f90ac432c3
Nathália P. Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-05-21 23:33:46 -0300

    Feat: Test case 2
    - Tabuleiro invalido (mais de 8 rainhas)

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -15,3 +15,17 @@ TEST_CASE("Tabuleiro válido", "[verificar_solucao]") {
     };
     REQUIRE(verificar_solucao(tabuleiro) == 1);
 }
+
+TEST_CASE("Tabuleiro inválido (mais de 8 rainhas)", "[verificar_solucao]") {
+    const char tabuleiro[TAMANHO][TAMANHO] = {
+        {'0', '0', '0', '0', '1', '0', '0', '0'},
+        {'0', '1', '0', '0', '1', '0', '0', '0'},
+        {'0', '0', '0', '1', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0', '1', '0'},
+        {'0', '0', '1', '0', '0', '0', '0', '0'},
+        {'0', '0', '0', '0', '0', '0', '0', '1'},
+        {'0', '0', '0', '0', '0', '1', '0', '0'},
+        {'1', '0', '0', '0', '0', '0', '0', '0'}
+    };
+    REQUIRE(verificar_solucao(tabuleiro) == -1);
+}
